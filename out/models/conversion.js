7a8ab0347f317a590cde6eaeb92bbf3dc92e5927
Neha <nabrol@wayfair.com>
2021-07-05 11:41:13 -0400

    remove out directory

--- out/models/conversion.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PropertyToConvert = void 0;
-var PropertyToConvert;
-(function (PropertyToConvert) {
-    PropertyToConvert["Length"] = "length";
-    PropertyToConvert["Mass"] = "mass";
-    PropertyToConvert["Volume"] = "volume";
-})(PropertyToConvert = exports.PropertyToConvert || (exports.PropertyToConvert = {}));
